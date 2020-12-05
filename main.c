f111cf5d3edb3928c88f064444101b107df70464
tshinhar <tshinhar@gmail.com>
2020-12-05 21:56:42 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,16 @@
 #include "Queue.h"
 #include "utilities.h"
 
-int main(int argc, char* argv) {
 
+int main(int argc, char* argv[]) {
+	if (argc != 5) {
+		printf("incorrect number of arguments");
+		exit(-1);
+	}
+	int num_of_threads, num_of_tasks;
+	num_of_threads = atoi(argv[4]);
+	num_of_tasks = atoi(argv[3]);
+	// load tasks file to the queue
+	Node* head = file_to_queue(argv[2], num_of_tasks);
+	return 0;
 }
